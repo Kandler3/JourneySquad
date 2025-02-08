f6aee39575dfc2d1f275f66f1b4d5010cd3a69b9
Kandler3 <artchuk@icloud.com>
2025-02-08 18:35:50 +0300

    init

---
+++ miniapp/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,45 @@
+import { Icon28AddCircle } from "@telegram-apps/telegram-ui/dist/icons/28/add_circle"
+import { Icon28Home } from "@/iclons/Home";
+import { Icon28Search } from "@/iclons/Search"
+import { Icon28Profile } from "@/iclons/Profile";
+
+export interface INavBarProps {
+}
+
+export function NavBar(props: INavBarProps) {
+    const navBarStyle: React.CSSProperties = {
+      display: 'flex',
+      justifyContent: 'space-around', // Распределяем кнопки равномерно
+      alignItems: 'center',
+      width: '100%',
+      padding: '10px 0',
+      backgroundColor: '#f2f2f2',
+      position:'absolute',
+      bottom: 0,
+    };
+  
+    const buttonStyle: React.CSSProperties = {
+      border: 'none',
+      background: 'transparent',
+      cursor: 'pointer',
+      outline: 'none',
+    };
+  
+    return (
+      <div style={navBarStyle}>
+        <button style={buttonStyle}>
+          <Icon28Home />
+        </button>
+        <button style={buttonStyle}>
+          <Icon28Search />
+        </button>
+        <button style={buttonStyle}>
+          <Icon28AddCircle />
+        </button>
+        <button style={buttonStyle}>
+          <Icon28Profile />
+        </button>
+      </div>
+    );
+  }
+  
