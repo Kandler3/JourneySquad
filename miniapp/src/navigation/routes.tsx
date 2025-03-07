e6d5491824b61c68b78766e72a008b81bdae622a
evelonk <evel.kuptsevich@gmail.com>
2025-03-08 00:52:47 +0300

    add profile page (will be edit)

--- miniapp/src/navigation/routes.tsx
+++ miniapp/src/navigation/routes.tsx
@@ -1,13 +1,12 @@
 import type { ComponentType, JSX } from 'react';
-
 import { IndexPage } from '@/pages/template_pages/IndexPage/IndexPage';
 import { InitDataPage } from '@/pages/template_pages/InitDataPage.tsx';
 import { LaunchParamsPage } from '@/pages/template_pages/LaunchParamsPage.tsx';
 import { ThemeParamsPage } from '@/pages/template_pages/ThemeParamsPage.tsx';
 import {HomePage} from "@/pages/Home/Home.tsx";
-import {ProfilePage} from "@/pages/Profile.tsx";
+import {UserProfilePage} from "@/pages/Profile/Profile.tsx";
 import {TravelPlanListPage} from "@/pages/TravelPlanList/TravelPlanList.tsx";
-import {TravelPlanViewPage} from "@/pages/TravelPlanView.tsx";
+import {TravelPlanViewPage} from "@/pages/TravelPlanView/TravelPlanView.tsx";
 import {TravelPlanEditPage} from "@/pages/TravelPlanEdit.tsx";
 
 interface Route {
@@ -23,7 +22,7 @@ export const routes: Route[] = [
   { path: '/theme-params', Component: ThemeParamsPage, title: 'Theme Params' },
   { path: '/launch-params', Component: LaunchParamsPage, title: 'Launch Params' },
   { path: '/home', Component: HomePage, title: 'Home' },
-  { path: '/profile/:id', Component: ProfilePage, title: 'Profile' },
+  { path: '/profile/:id', Component: UserProfilePage, title: 'Profile' },
   { path: '/travel-plans', Component: TravelPlanListPage, title: 'Travel Plans' },
   { path: '/travel-plans/:id', Component: TravelPlanViewPage, title: 'Travel Plan' },
   { path: '/travel-plans/:id/edit', Component: TravelPlanEditPage, title: 'Travel Plan' },
