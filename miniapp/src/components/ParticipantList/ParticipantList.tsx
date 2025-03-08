473a9aaa5f4ca82bff764287a90fa7e8d921851a
evelonk <evel.kuptsevich@gmail.com>
2025-03-08 14:39:01 +0300

    add ParticipantList component

--- miniapp/src/components/ParticipantList/ParticipantList.tsx
+++ miniapp/src/components/ParticipantList/ParticipantList.tsx
@@ -1,22 +1,37 @@
-import { FC } from "react";
-import { ParticipantCard } from "@/components/ParticipantCard/ParticipantCard.tsx";
-import { Participant } from "@/types/Participant";
-import "./ParticipantList.css";
+import React from "react";
+import { ParticipantCard } from "@/components/ParticipantCard/ParticipantCard";
+import { ContentSection } from "@/components/ContentSection/ContentSection";
+import "./ParticipantList.css"; 
 
-interface ParticipantListProps {
-    participants: Participant[];
+interface Participant {
+    id: string;
+    name: string;
+    photoUrl: string;
 }
 
-export const ParticipantList: FC<ParticipantListProps> = ({ participants }) => {
+interface ParticipantsListProps {
+    participants: Participant[];
+    onParticipantClick: (participantId: string) => void;
+}
+
+export const ParticipantsList: React.FC<ParticipantsListProps> = ({ participants, onParticipantClick }) => {
     return (
-        <div className= "list">
-            {participants.map((participant) => (
-                <ParticipantCard
-                    key={participant.id}
-                    name={participant.name}
-                    photoUrl={participant.photoUrl}
-                />
-            ))}
+        <div className="headerContainer">
+            <h2 className="participantsTitle">Участники</h2>
+            <ContentSection>
+                {participants.map((participant) => (
+                    <div
+                        key={participant.id}
+                        onClick={() => onParticipantClick(participant.id)}
+                        style={{ cursor: "pointer" }}
+                    >
+                        <ParticipantCard
+                            name={participant.name}
+                            photoUrl={participant.photoUrl}
+                        />
+                    </div>
+                ))}
+            </ContentSection>
         </div>
     );
 };
