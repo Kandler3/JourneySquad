d0cd0b19acecf0580a561178c68978eb6d52359e
Kandler3 <artchuk@icloud.com>
2025-03-14 14:45:30 +0300

    fixed travel plan from not updating on photo adding. service returns empty array in array fetches if api returns nill

--- miniapp/src/components/TravelPlanForm/TravelPlanForm.tsx
+++ miniapp/src/components/TravelPlanForm/TravelPlanForm.tsx
@@ -95,7 +95,7 @@ export const TravelPlanForm : FC<TravelPlanFormProps> = ({travelPlan, onSubmit, 
             for (let i = 0; i < e.target.files.length; i++)
             {
                 const resp = await uploadFile(e.target.files[i])
-                photos.push(new TravelPlanPhoto(photos.length, resp))
+                setPhotos([...photos, new TravelPlanPhoto(photos.length, resp)])
             }
         }
     }
